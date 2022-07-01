773a2da6612c1f60c5bca30d46c432b7cb4eff24
e2f5db0 <sami.toiskallio@protonmail.com>
2022-07-01 18:41:35 +0300

    Include frontend tests to the pipeline & set cypress timeout in pipeline.yaml

--- backend/cypress.config.js
+++ backend/cypress.config.js
@@ -4,8 +4,5 @@ module.exports = defineConfig({
   e2e: {
     baseUrl: 'http://localhost:5000',
     video: false,
-    // Set ridiculously high because deployment
-    // pipeline takes a long time and will timeout otherwise
-    defaultCommandTimeout: 480 * 1000
   },
 })
--- backend/cypress/e2e/pagesCanBeOpened.cy.js
+++ backend/cypress/e2e/pagesCanBeOpened.cy.js
@@ -1,12 +1,12 @@
 /* eslint-disable no-undef */
 describe('React Node Template', function () {
   it('Front page can be opened', function () {
-    cy.visit('/', { timeout: 480 * 1000 })
+    cy.visit('/')
     cy.contains('React Node Template')
     cy.contains('containerized edition')
   })
   it('Navbar can be used to navigate', function () {
-    cy.visit('/', { timeout: 480 * 1000 })
+    cy.visit('/')
     cy.get('#firstOption').click()
     cy.contains('First Option')
   })
